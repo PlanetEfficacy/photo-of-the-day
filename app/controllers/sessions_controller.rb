0a350203f1f5cc9768d9c1f11a8b61c777865ef6
PlanetEfficacy <jspevack@gmail.com>
2016-12-26 12:07:12 -0700

    Add default user can't access photo new

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    user = User.update_or_create(env["omniauth.auth"])
+    user = User.update_or_create(request.env["omniauth.auth"])
     session[:user_id] = user.id
     redirect_to user.admin? ? new_photo_path : photos_path
   end
